7650af2177004b3531cfb43efb3b9d0104a47af2
mccarthyr <mccarthy.richard@gmail.com>
2018-02-19 19:41:30 +0000

    Added Bootstrap responsive CSS Framework

--- UI/angular-jwt-spring-athlete/src/app/app-routing.module.ts
+++ UI/angular-jwt-spring-athlete/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { DashboardComponent }   from './dashboard/dashboard.component';
 import { AthleteComponent }     from './athlete/athlete.component';
+import { LoginComponent }       from './login/login.component';
 
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'athlete', component: AthleteComponent }
+  { path: 'athlete', component: AthleteComponent },
+  { path: 'login', component: LoginComponent }
 ];
 
 

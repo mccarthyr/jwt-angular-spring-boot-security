60033d630e6be2b4a66829e606cc1004f30b191c
mccarthyr <mccarthy.richard@gmail.com>
2018-04-25 14:22:34 +0100

    Web CORS config and JWT Authentication Filter additions

---
+++ API/athlete/src/main/java/com/fireduptech/spring/rest/jwt/athlete/config/WebCorsConfig.java
@@ -0,0 +1,23 @@
+package com.fireduptech.spring.rest.jwt.athlete.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+/*
+@Configuration
+public abstract class WebCorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMapping(CorsRegistry corsRegistry) {
+        corsRegistry.addMapping( "/**" )
+                .allowedOrigins( "http://localhost:4200" )
+                .allowedMethods( "GET", "POST", "DELETE" )
+                .allowedHeaders( "*" )
+                .allowCredentials( true )
+                .maxAge( 3600 );
+    }
+
+
+}
+*/
--- API/athlete/src/main/java/com/fireduptech/spring/rest/jwt/athlete/security/JwtAuthenticationFilter.java
+++ API/athlete/src/main/java/com/fireduptech/spring/rest/jwt/athlete/security/JwtAuthenticationFilter.java
@@ -1,5 +1,7 @@
 package com.fireduptech.spring.rest.jwt.athlete.security;
 
+import org.springframework.http.HttpMethod;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -41,12 +43,31 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 
 
     @Override
+    protected boolean requiresAuthentication(HttpServletRequest request, HttpServletResponse response) {
+
+        if ( super.requiresAuthentication( request, response ) ) {
+
+            if ( HttpMethod.OPTIONS.name().equalsIgnoreCase( request.getMethod() ) ) {
+                return false;
+            }
+            Authentication existingAuth = SecurityContextHolder.getContext().getAuthentication();
+
+            if ( existingAuth == null || !existingAuth.isAuthenticated() ) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+
+    @Override
     public Authentication attemptAuthentication( HttpServletRequest req, HttpServletResponse res ) throws AuthenticationException {
 
         try {
             String username = obtainUsername( req );
-            String password = obtainPassword( req )
-;
+            String password = obtainPassword( req );
+
             //User creds = new ObjectMapper().readValue(req.getInputStream(), User.class);
 
             return authenticationManager.authenticate(
@@ -61,39 +82,13 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
         }
     }
 
-    /*
-    @Override
-    public Authentication attemptAuthentication( HttpServletRequest req, HttpServletResponse res ) throws AuthenticationException {
-
-        try {
-            User creds = new ObjectMapper().readValue(req.getInputStream(), User.class);
-
-            return authenticationManager.authenticate(
-                    new UsernamePasswordAuthenticationToken(
-                            creds.getUsername(),
-                            creds.getPassword(),
-                            new ArrayList<>())
-            );
-
-        } catch ( IOException e ) {
-            throw new RuntimeException( e );
-        }
-    }
-    */
-
 
     @Override
     protected void successfulAuthentication( HttpServletRequest req,
                                             HttpServletResponse res,
                                             FilterChain chain,
                                             Authentication auth ) throws IOException, ServletException {
-        /*
-        String token = Jwts.builder()
-                .setSubject(((User) auth.getPrincipal()).getUsername())
-                .setExpiration( new Date( System.currentTimeMillis() + EXPIRATION_TIME ) )
-                .signWith( SignatureAlgorithm.HS512, SECRET )
-                .compact();
-        */
+
         String token = Jwts.builder()
                 .setSubject(((User) auth.getPrincipal()).getUsername())
                 .setIssuer( "http://localhost:8080" )
@@ -106,14 +101,3 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 
 
 }
-
-/*
-spring.security.jwt.tokenIssuer=http://localhost:8080
-    String token = Jwts.builder()
-            .setClaims(claims)
-            .setIssuer(settings.getTokenIssuer())
-            .setIssuedAt(Date.from(currentTime))
-            .setExpiration(Date.from(currentTime.plus(settings.getTokenExpirationTime(), ChronoUnit.MINUTES)))
-            .signWith(SignatureAlgorithm.HS512, settings.getTokenSigningKey())
-            .compact();
-*/
